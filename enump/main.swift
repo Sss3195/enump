42e8d5e6972d9a8a2345474f4a38c2b49918aaaa
Sss3195 <s20171103195@danqianfang.local>
2018-11-07 09:09:23 +0800

    枚举
    简单例子

--- enump/main.swift
+++ enump/main.swift
@@ -7,6 +7,41 @@
 //
 
 import Foundation
+enum Daysofaweek {
+    case Sunday
+    case Monday
+    case Tuesday
+    case Wednesday
+    case Thursday
+    case Friday
+    case Saturday
+}
 
-print("Hello, World!")
+var weekday = Daysofaweek.Thursday
+weekday = .Monday
+switch weekday {
+case .Sunday:
+    print("7")
+case .Monday:
+    print("1")
+case .Tuesday:
+    print("2")
+case .Wednesday:
+    print("3")
+case .Thursday:
+    print("4")
+case .Friday:
+    print("5")
+case .Saturday:
+    print("6")
+default:
+    print("0")
+}
+enum Month: Int {
+    case January = 1, February, March, April, May, June, July, August, September, October, November, December
+}
+
+let yearMonth = Month.April.rawValue
+print("数字月份为: \(yearMonth)。")
+
 
